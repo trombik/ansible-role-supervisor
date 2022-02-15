85daa832d552fa5a2d4e4665bb20d9dfc8e9a30e
Tomoyuki Sakurai <y@trombik.org>
2022-02-15 10:02:54 +0700

    feat: initial implementation

--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -1,49 +1,122 @@
 require "spec_helper"
 require "serverspec"
 
-package = "supervisor"
-service = "supervisor"
-config  = "/etc/supervisor/supervisor.conf"
-user    = "supervisor"
-group   = "supervisor"
-ports   = [PORTS]
+package = case os[:family]
+          when "freebsd"
+            "sysutils/py-supervisor"
+          else
+            raise "unknown os[:family]: `#{os[:family]}`"
+          end
+service = case os[:family]
+          when "freebsd"
+            "supervisord"
+          end
+config_dir = case os[:family]
+             when "freebsd"
+               "/usr/local/etc/supervisor"
+             else
+               "/etc/supervisor"
+             end
+conf_d_dir = "#{config_dir}/conf.d"
+config  = "#{config_dir}/supervisord.conf"
+user    = "root"
+group   = case os[:family]
+          when "freebsd", "openbsd"
+            "wheel"
+          else
+            "root"
+          end
+ports   = []
 log_dir = "/var/log/supervisor"
-db_dir  = "/var/lib/supervisor"
-
-case os[:family]
-when "freebsd"
-  config = "/usr/local/etc/supervisor.conf"
-  db_dir = "/var/db/supervisor"
-end
+unix_socket_dir = case os[:family]
+                  when "freebsd"
+                    "/var/run/supervisor"
+                  end
+unix_socket_file = "#{unix_socket_dir}/supervisor.sock"
+pid_dir = case os[:family]
+          when "freebsd"
+            "/var/run/supervisor"
+          end
 
 describe package(package) do
   it { should be_installed }
 end
 
-describe file(config) do
+describe file config_dir do
+  it { should exist }
+  it { should be_directory }
+  it { should be_owned_by user }
+  it { should be_grouped_into group }
+  it { should be_mode 755 }
+end
+
+describe file conf_d_dir do
+  it { should exist }
+  it { should be_directory }
+  it { should be_owned_by user }
+  it { should be_grouped_into group }
+  it { should be_mode 755 }
+end
+
+describe file conf_d_dir do
+  it { should exist }
+  it { should be_directory }
+  it { should be_owned_by user }
+  it { should be_grouped_into group }
+  it { should be_mode 755 }
+end
+
+describe file pid_dir do
+  it { should exist }
+  it { should be_directory }
+  it { should be_owned_by user }
+  it { should be_grouped_into group }
+  it { should be_mode 755 }
+end
+
+describe file unix_socket_dir do
+  it { should exist }
+  it { should be_directory }
+  it { should be_owned_by user }
+  it { should be_grouped_into group }
+  it { should be_mode 755 }
+end
+
+describe file unix_socket_file do
+  it { should exist }
+  it { should be_socket }
+  it { should be_owned_by user }
+  it { should be_grouped_into group }
+  it { should be_mode 700 }
+end
+
+describe file config do
+  it { should exist }
   it { should be_file }
-  its(:content) { should match Regexp.escape("supervisor") }
-end
-
-describe file(log_dir) do
-  it { should exist }
-  it { should be_mode 755 }
   it { should be_owned_by user }
   it { should be_grouped_into group }
-end
-
-describe file(db_dir) do
-  it { should exist }
-  it { should be_mode 755 }
-  it { should be_owned_by user }
-  it { should be_grouped_into group }
+  it { should be_mode 644 }
+  its(:content) { should match Regexp.escape("Managed by ansible") }
 end
 
 case os[:family]
 when "freebsd"
-  describe file("/etc/rc.conf.d/supervisor") do
+  describe file "/etc/rc.conf.d/#{service}" do
+    it { should exist }
     it { should be_file }
+    it { should be_owned_by user }
+    it { should be_grouped_into group }
+    it { should be_mode 644 }
+    its(:content) { should match Regexp.escape("Managed by ansible") }
   end
+end
+
+describe file log_dir do
+  it { should exist }
+  it { should be_directory }
+  it { should be_mode 755 }
+  it { should be_owned_by user }
+  it { should be_grouped_into group }
 end
 
 describe service(service) do
